81b0d6ada3b122372a1829017a2930fcfe1a87ec
Hyper Engine <84406707+Crayz310@users.noreply.github.com>
2023-08-04 14:22:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
+const isSafari = /^((?!chrome | android).)*safari/i.test(navigator.userAgentData);
 const fab = document.getElementById('fab');
 const btn = document.querySelector('.btn');
 
-function isOutOfView(element) {
-	const rect = element.getBoundingClientRect();
-	return (
-		rect.bottom < 0 ||
-		rect.right < 0 ||
-		rect.left > window.innerWidth ||
-		rect.top > window.innerHeight
-	);
-}
-
-function handleFabColor() {
-	if (isOutOfView(btn)) {
-		document.documentElement.style.setProperty('--stcolor', 'orange')
-		document.documentElement.style.setProperty('--hvcolor', 'darkorange')
-	} else {
-		document.documentElement.style.setProperty('--stcolor', 'black')
-		document.documentElement.style.setProperty('--hvcolor', 'black')
-
+if (!isSafari) {
+	function isOutOfView(element) {
+		const rect = element.getBoundingClientRect();
+		return (
+			rect.bottom < 0 ||
+			rect.right < 0 ||
+			rect.left > window.innerWidth ||
+			rect.top > window.innerHeight
+		);
 	}
+
+	function handleFabColor() {
+		if (isOutOfView(btn)) {
+			document.documentElement.style.setProperty('--stcolor', 'orange')
+			document.documentElement.style.setProperty('--hvcolor', 'darkorange')
+		} else {
+			document.documentElement.style.setProperty('--stcolor', 'black')
+			document.documentElement.style.setProperty('--hvcolor', 'black')
+
+		}
+	}
+
+	window.addEventListener('scroll', handleFabColor);
+	window.addEventListener('resize', handleFabColor);
+
+	handleFabColor();
 }
 
-window.addEventListener('scroll', handleFabColor);
-window.addEventListener('resize', handleFabColor);
-
-handleFabColor();
